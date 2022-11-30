a448b4bb08d597bd5d1faf0ad9385596f62e09e3
fzu-kwz <2550121932@qq.com>
2022-11-30 17:17:58 +0800

    create menu by router

--- src/App.vue
+++ src/App.vue
@@ -32,70 +32,22 @@ import {
   KButton,
 } from "$/index";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
 const backTopNode = ref();
 onMounted(() => {
   backTopNode.value = document.getElementsByTagName("main")[0];
 });
 
-const menu = [
-  {
-    route: "/container",
-    title: "Container 布局容器",
-  },
-  {
-    route: "/button",
-    title: "Button 按钮",
-  },
-  {
-    route: "/radio",
-    title: "Radio 单选框",
-  },
-  {
-    route: "/input",
-    title: "Input 输入框",
-  },
-  {
-    route: "/select",
-    title: "Select 下拉框",
-  },
-  {
-    route: "/form",
-    title: "Form 表单",
-  },
-  {
-    route: "/menu",
-    title: "Menu 菜单栏",
-  },
-  {
-    route: "/pagination",
-    title: "Pagination 分页",
-  },
-  {
-    route: "/loading",
-    title: "Loading 加载中",
-  },
-  {
-    route: "/upload",
-    title: "Upload 上传",
-  },
-  {
-    route: "/progress",
-    title: "Progress 进度条",
-  },
-  {
-    route: "/empty",
-    title: "Empty 空状态",
-  },
-  {
-    route: "/lazy-load-img",
-    title: "LazyLoadImg 懒加载图片",
-  },
-  {
-    route: "/back-top",
-    title: "BackTop 回到顶部",
-  },
-];
+const router = useRouter();
+const menu = router
+  .getRoutes()
+  .filter((item) => {
+    return item.meta.title;
+  })
+  .map((item) => {
+    return { route: item.path, title: item.meta.title };
+  });
 
 const open = ref(false);
 </script>
