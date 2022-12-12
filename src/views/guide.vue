d844cb73f360dc5cf704ca54d8574307c2ad4b5b
KK <2550121932@qq.com>
2022-12-12 12:50:14 +0800

    Update guide.vue

--- src/views/guide.vue
+++ src/views/guide.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="guide">
-    <h2>KUI for Vue3 + Ts</h2>
+    <h2>KUI based on Vue3 and TypeScript</h2>
+    <p>Usage: npm i k-vue3-comp</p>
     <p>
       Some components refer to
-      <a href="https://element.eleme.cn/#/zh-CN" target="_blank">Element</a>
-      , more components continue to be developed.
+      <a href="https://element.eleme.cn/#/zh-CN" target="_blank">Element</a>.
+      More components continue to be developed.
     </p>
     <p>
       If you have any questions, please go to
       <a href="https://github.com/fzu-kwz/UI/issues" target="_blank">
         issues
       </a>
-      or send an email to 1358848173@qq.com. Thank you!
+      or send an email to <b>1358848173@qq.com</b>. Thank you!
     </p>
     <div>
       <Card class="guide-card">
@@ -47,7 +48,7 @@ import { Card } from "$/index";
   a {
     color: #000;
     text-decoration: none;
-    font-weight: 600;
+    font-weight: bold;
 
     &:hover {
       text-decoration: underline;
