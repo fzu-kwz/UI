e633bca28fd07be2bdbe5348f7dc09638aaddca0
KK <2550121932@qq.com>
2022-12-21 14:07:24 +0800

    usage updated

--- src/views/pagination.vue
+++ src/views/pagination.vue
@@ -1,15 +1,6 @@
 <template>
   <KForm align-top>
     <h3 style="margin-top: 0;">Pagination 分页</h3>
-    <p class="usage">
-      需要绑定v-model:current: 当前页码（number，默认1）<br />
-      total: 总数（number，默认20）<br />
-      jumper: 是否显示跳转（boolean）<br />
-      background: 是否显示背景色（boolean）<br />
-      @currentChange: 页码改变触发（回调参数当前页码）<br />
-      @prevClick: 点击上一页按钮触发（回调参数当前页码）<br />
-      @nextClick: 点击下一页按钮触发（回调参数当前页码）
-    </p>
     <FormItem label-text="基础用法">
       <Pagination v-model:current="current" :total="5" jumper></Pagination>
     </FormItem>
