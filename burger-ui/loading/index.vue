0c9d941cf32bdb2654ee12e7dcc91e4583dc55b7
KK <2550121932@qq.com>
2023-01-11 10:56:41 +0800

    loading updated

--- burger-ui/loading/index.vue
+++ burger-ui/loading/index.vue
@@ -19,25 +19,46 @@
 <script lang="ts">
 export default {
   name: "Loading",
-  props: {
-    target: {
-      type: [String, HTMLElement],
-      default: "body",
-    },
-    loading: {
-      type: Boolean,
-      default: true,
-    },
-    text: {
-      type: String,
-      default: "加载中",
-    },
-    background: {
-      type: String,
-      default: "",
-    },
-  },
 };
+</script>
+
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  target: {
+    type: [String, HTMLElement],
+    default: "body",
+  },
+  loading: {
+    type: Boolean,
+    default: true,
+  },
+  text: {
+    type: String,
+    default: "加载中",
+  },
+  background: {
+    type: String,
+    default: "",
+  },
+  delay: {
+    type: Number,
+    defaule: 0,
+  },
+});
+
+const loading = ref(props.loading);
+
+watch(props, () => {
+  if (!props.loading) {
+    setTimeout(() => {
+      loading.value = false;
+    }, props.delay);
+  } else {
+    loading.value = true;
+  }
+});
 </script>
 
 <style lang="less" scoped>
